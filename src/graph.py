1ac613ea3ae655520d6f92106e5776468807d271
Logan Katz <loganzk@gmail.com>
2016-09-29 13:30:26 -0700

    Trying annotated graphs

--- src/graph.py
+++ src/graph.py
@@ -5,41 +5,54 @@ import sys
 import os
 import json
 import warnings
+import summarize
 
 warnings.simplefilter('ignore', np.RankWarning)
 
 def main(models):
-    f, ax = plt.subplots()
-    ax.set_ylabel('Loss')
-    ax.set_xlabel('Batch number')
-    for model_folder in models:
-        make_graph(model_folder)    
+    #summarize.main(models)
+    f, ax = plt.subplots(len(models), 2, sharex=True, sharey=True) 
+    try:
+        test = ax.shape[1]
+    except:
+        ax = ax[np.newaxis]
+    for idx, model_folder in enumerate(models):
+        make_graph(model_folder, ax, idx)    
+    f.subplots_adjust(hspace=0)
 
-def make_graph(folder_num):
+def make_graph(folder_num, ax_list, ax_idx):
+    #ax_list[ax_idx, 0].set_ylabel('Loss')
+    #ax.set_xlabel('Batch number')
+    #ax.set_title('Run-{} Loss Graphs'.format(folder_num))
     model_fp = './models/run-{}/'.format(folder_num)
     runs = os.listdir(model_fp)
     summary_fp = runs.pop(0)
-    with open(model_fp + 'run_summary.json', 'r') as j:
-        summary = json.load(j)
-    for idx, run_fp in enumerate(runs):
-        print '- '*10
-        print 'Summary for run {}.'.format(idx)
-        for key, val in summary[str(idx)].iteritems():
-            print '{}: {}'.format(key,val)
-        print '- '*10
-        graph_numpy(model_fp + 'train-{}.npy'.format(idx), idx)
 
-def graph_numpy(fp, idx):
+    add_annotation(model_fp, ax_list[ax_idx, 1])
+    for idx, run_fp in enumerate(runs):
+        graph_numpy(model_fp + 'train-{}.npy'.format(idx), idx, ax_list[ax_idx, 0])
+
+def graph_numpy(fp, idx, ax):
     colors = sbn.color_palette('muted')
     arr = np.load(fp)
     mean = arr.mean(axis=1)
     batch = np.arange(len(mean))
-    fit = np.poly1d(np.polyfit(batch,mean,30))
+    fit = np.poly1d(np.polyfit(batch,mean,50))
 
-    plt_label = 'Run - {}'.format(idx)
-    plt.plot(batch,fit(batch), c=colors[idx], label=plt_label)
-    plt.plot(batch, mean, c=colors[idx], alpha=0.2)
-    plt.legend()
+    plt_label = 'Subrun-{}'.format(idx)
+    ax.plot(batch,fit(batch), c=colors[idx], label=plt_label)
+    ax.plot(batch, mean, c=colors[idx], alpha=0.2)
+    ax.legend()
+
+def add_annotation(fp, ax):
+    pos = ax.get_position()
+    ax.axis('off')
+    summary_fp = fp + 'run_summary.json'
+    with open(summary_fp, 'r') as j:
+        summary = json.load(j)
+    print pos.x0
+    print pos.y1
+    ax.text(0.5, 0.5, 'blah')
 
 if __name__ == '__main__':
     models = sys.argv[1:] 
---
+++ src/summarize.py
@@ -0,0 +1,30 @@
+import numpy as np
+import sys
+import os
+import json
+
+def main(models):
+    print '\n' + '--'*10
+    print 'Summarizing runs {}.'.format(', '.join(models))
+    print '--'*10 
+
+    for model_folder in models:
+        print '\n* * * Run {} * * * '.format(model_folder)
+        summarize(model_folder)    
+
+def summarize(folder_num):
+    model_fp = './models/run-{}/'.format(folder_num)
+    runs = os.listdir(model_fp)
+    summary_fp = runs.pop(0)
+    with open(model_fp + 'run_summary.json', 'r') as j:
+        summary = json.load(j)
+    for idx, run_fp in enumerate(runs):
+        print '- '*10
+        print 'Summary for training sub-run {}.'.format(idx)
+        for key, val in summary[str(idx)].iteritems():
+            print '{}: {}'.format(key,val)
+        print '- '*10
+
+if __name__ == '__main__':
+    models = sys.argv[1:] 
+    main(models)
