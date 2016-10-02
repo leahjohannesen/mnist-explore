d0372a7066baf779276d7db35816039b28891f97
Logan Katz <loganzk@gmail.com>
2016-10-01 23:04:47 -0700

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# mnist-explore
+# MNIST Exploration
+This repo contains an exploration of the MNIST data set using Tensorflow.<br>
+Several factors are being explored:
+* Accuracy impacts of various types of model architectures/layers including -
+  * Number of layers
+  * Convolutions vs dropout
+  * Dropout
+  * Batch normalization
+  * Activation functions
+  * Different optimization functions
+* The effects of gaussian noise of classification performance
+* The effects of mislabeled data on the classification performance
+
+## Repo Structure
+* Exploration - Contains markdown files with Q/A and model accuracy evaluations
+* Images - Contains saved images for analysis
+* Models - The saved jsons/numpy arrays of run performances
+* src - The scripts used in the analysis
+  * pymodels - Folder containing python files, one for each model
+  * datagen.py - Data generator/augmenter
+  * eval.py - The core training/testing module
+  * graph.py - Used to visualize the results from the training/testing
+  * summarize.py - Small utility used to quickly see the hyperparameters used in each run
+  * utils.py - Utilities for saving models
--- src/eval.py
+++ src/eval.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import os
 import utils
-from models.opts import opts
+from pymodels.opts import opts
 
 '''
 The main file that runs the training of the net.
@@ -12,12 +12,12 @@ Required sysargs:
     model - first sysarg should be the model that you want to run, ex "basic" for basic.py
 Current optional sysargs:
     grid - performs a grid search as described in _grid()
-    log - logs the training information into a json for parameters and 
+    log - logs the training information into a json for parameters and
         numpy arrays for loss/test results
-    aug_noise/aug_miss - uses augmented data as described in 
-        datagen.py, must be followed the value of augment 
+    aug_noise/aug_miss - uses augmented data as described in
+        datagen.py, must be followed the value of augment
         ex. aug_noise 8 - will produce augmented data with gaussian noise with mean=0, stdev=8
-    sgd/momentum/adadelta/adagrad - adds another optimizer to replace adam 
+    sgd/momentum/adadelta/adagrad - adds another optimizer to replace adam
 '''
 
 def main(model, other):
@@ -57,18 +57,18 @@ def _grid(model_dir, model, other):
 def _train(model_dir, model, other, lr=1e-4, drop=0.5):
     tf.reset_default_graph()
     tf.set_random_seed(1)
-    
+
     data = _data(other)
 
     x = tf.placeholder(tf.float32, shape=[None, 784])
     y = tf.placeholder(tf.float32, shape=[None, 10])
     keep = tf.placeholder(tf.float32)
-    
+
     #Nifty way I found to dynamically import models
     #Each model file should contain a pred, acc, and acc_cass function
-    sys.path.append('/home/ubuntu/mnist-explore/src/models/')
+    sys.path.append('/home/ubuntu/mnist-explore/src/pymodels/')
     mod = __import__(model)
-    
+
     y_pred = mod.pred(x, keep)
     acc = mod.acc(y, y_pred)
     acc_class = mod.acc_class(y, y_pred)
@@ -80,10 +80,10 @@ def _train(model_dir, model, other, lr=1e-4, drop=0.5):
 
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
-        
+
         #Some constats/variables used in the training
-        epochs = 1 
-        batch_size = 32 
+        epochs = 1
+        batch_size = 32
         total_train = data.x_train.shape[0]
         deciles = int(total_train/(10*batch_size))
 
@@ -116,7 +116,7 @@ def _train(model_dir, model, other, lr=1e-4, drop=0.5):
                         batch_val = data.next_batch_val(batch_size)
                         if not batch_val:
                             break
-                        val_acc = sess.run(acc, feed_dict={x: batch_val[0], y: batch_val[1], 
+                        val_acc = sess.run(acc, feed_dict={x: batch_val[0], y: batch_val[1],
                                                            keep: 1.0})
                         val_list.append(val_acc)
                     print "End of Epoch"
@@ -127,7 +127,7 @@ def _train(model_dir, model, other, lr=1e-4, drop=0.5):
                 if n%deciles == 0:
                     print "Percent of epoch complete: {}0%.".format(n/deciles)
                 n += 1
-                loss_val, _ = sess.run([loss, train_step], feed_dict={x: batch[0], 
+                loss_val, _ = sess.run([loss, train_step], feed_dict={x: batch[0],
                                                                   y: batch[1], keep: drop})
                 loss_list.append(loss_val)
 
@@ -152,11 +152,11 @@ def _train(model_dir, model, other, lr=1e-4, drop=0.5):
         test_total_acc = np.append(test_list_acc, test_class_acc)
 
     #If log was called, model_dir will exist. If it does, pass all the info
-    #into the utility that saves all the hyper parameters 
+    #into the utility that saves all the hyper parameters
     if model_dir:
-        utils.save_train(model_dir, loss_list, model, data.aug, data.aug_val, 
+        utils.save_train(model_dir, loss_list, model, data.aug, data.aug_val,
                            lr, batch_size, drop, opt_val)
-        utils.save_test(model_dir, test_total_acc, model, data.aug, data.aug_val, 
+        utils.save_test(model_dir, test_total_acc, model, data.aug, data.aug_val,
                            lr, batch_size, drop, opt_val)
 
 if __name__ == '__main__':
